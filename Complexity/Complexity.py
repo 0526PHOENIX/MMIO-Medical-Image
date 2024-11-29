99134bed5f63b30f1bd1ee9fa0adda92cddc3ddb
Lab_Local <0526phoenix@gmail.com>
2024-11-29 18:11:03 +0800

    1811

--- Complexity/Complexity.py
+++ Complexity/Complexity.py
@@ -4,6 +4,7 @@ Package
 ========================================================================================================================
 """
 import time
+import math
 
 from typing import Literal
 
@@ -12,7 +13,7 @@ from torch import nn
 from torch import Tensor
 from torch.nn import Module
 
-
+from torchvision import models
 """
 ========================================================================================================================
 Complexity Calculator
@@ -32,7 +33,7 @@ class Complexity():
         self.dummy = dummy
 
         # FLOPs
-        self.flop = 0
+        self.flops = 0
 
         # Calculate FLOPs
         self.hook_layer()
@@ -52,101 +53,70 @@ class Complexity():
         ----------------------------------------------------------------------------------------------------------------
         """
         def forward_hook(module: Module, feature_in: tuple[Tensor], feature_out: Tensor) -> None:
-            
+
             # Conver Tuple to Tensor
             feature_in = feature_in[0]
 
             # Convolution 2D & Transpose Convolution 2D
             if isinstance(module, (nn.Conv2d, nn.ConvTranspose2d)):
-                # Input and Output Channels
+                # Input Channels
                 channel_in = feature_in.shape[1]
-                # Output Channels, Height and Width
-                channel_out, height_out, width_out = feature_out.shape[1:]
                 # Kernel Size
                 kernel = module.kernel_size[0]
                 # FLOPs
-                flop = 2 * kernel * kernel * channel_in * channel_out * height_out * width_out
+                flops = 2 * kernel * kernel * channel_in * feature_out.numel()
 
             # Fully Connected
             elif isinstance(module, nn.Linear):
                 # FLOPs
-                flop = 2 * feature_in.shape[1] * feature_out.shape[1]
+                flops = 2 * feature_in.numel() * feature_out.numel()
 
-            # Batch Normalization 1D
-            elif isinstance(module, nn.BatchNorm1d):
-                # Output Channels
-                channel_out = feature_out.shape[1]
+            # Batch Normalization
+            elif isinstance(module, (nn.BatchNorm1d, nn.BatchNorm2d)):
                 # FLOPs
-                flop = 4 * channel_out
+                flops = 4 * feature_out.numel()
 
-            # Batch Normalization 2D
-            elif isinstance(module, nn.BatchNorm2d):
-                # Output Channels, Height and Width
-                channel_out, height_out, width_out = feature_out.shape[1:]
+            # Instance Normalization
+            elif isinstance(module, (nn.InstanceNorm1d, nn.InstanceNorm2d)):
                 # FLOPs
-                flop = 4 * channel_out * height_out * width_out
+                flops = 7 * feature_out.numel()
 
-            # Instance Normalization 1D
-            elif isinstance(module, nn.InstanceNorm1d):
-                # Output Channels and Length
-                channel_out, length_out = feature_out.shape[1:]
+            # Group Normalization
+            elif isinstance(module, nn.GroupNorm):
                 # FLOPs
-                flop = 7 * channel_out * length_out
-            
-            # Instance Normalization 2D
-            elif isinstance(module, nn.InstanceNorm2d):
-                # Output Channels, Height and Width
-                channel_out, height_out, width_out = feature_out.shape[1:]
-                # FLOPs
-                flop = 7 * channel_out * height_out * width_out
+                flops = 4 * feature_out.numel()
 
-            # # Layer Normalization
+            # # Layer Normalization (Self-Defined)
             # elif isinstance(module, LayerNorm):
-            #     # Output Channels, Height and Width
-            #     channel_out, height_out, width_out = feature_out.shape[1:]
             #     # FLOPs
-            #     flop = 7 * channel_out * height_out * width_out
+            #     flops = 7 * feature_out.numel()
 
-            # Dropout 1D
-            elif isinstance(module, nn.Dropout):
-                # Output Channels
-                channel_out = feature_out.shape[1]
+            # Dropout
+            elif isinstance(module, (nn.Dropout, nn.Dropout1d, nn.Dropout2d)):
                 # FLOPs
-                flop = 2 * channel_out
+                flops = 2 * feature_out.numel()
 
-            # Dropout 2D
-            elif isinstance(module, nn.Dropout2d):
-                # Output Channels, Height and Width
-                channel_out, height_out, width_out = feature_out.shape[1:]
+            # Activation (Only Estimation)
+            elif isinstance(module, (nn.ReLU, nn.LeakyReLU, nn.GELU, nn.SELU, nn.SiLU, nn.Tanh, nn.Sigmoid, nn.Softmax2d, nn.Softmax)):
                 # FLOPs
-                flop = 2 * channel_out * height_out * width_out
-
-            # Activation
-            elif isinstance(module, (nn.ReLU, nn.LeakyReLU, nn.GELU, nn.Tanh, nn.Sigmoid, nn.Softmax2d, nn.Softmax)):
-
-                # Fully Connected
-                if feature_out.dim() == 2:
-                    # Output Channels
-                    channel_out = feature_out.shape[1]
-                    # FLOPs
-                    flop = channel_out
-                # Convolution
-                elif feature_out.dim() == 4:
-                    # Output Channels, Height and Width
-                    channel_out, height_out, width_out = feature_out.shape[1:]
-                    # FLOPs
-                    flop = channel_out * height_out * width_out
-                else:
-                    raise TypeError('Invalid Number of Axis of Activation')
+                flops = 4 * feature_out.numel()
 
             # Pooling 
             elif isinstance(module, (nn.MaxPool2d, nn.AvgPool2d)):
-                # Output Channels, Height and Width
-                channel_out, height_out, width_out = feature_out.shape[1:]
                 # Kernel Size
                 kernel = module.kernel_size
                 # FLOPs
-                flop = kernel * kernel * channel_out * height_out * width_out
+                flops = kernel * kernel * feature_out.numel()
+
+            # Pooling
+            elif isinstance(module, (nn.AdaptiveAvgPool2d, nn.AdaptiveMaxPool2d)):
+                # Check Shape
+                if (feature_in.shape[2] != feature_in.shape[3] or feature_out.shape[2] != feature_out.shape[3]):
+                    raise TypeError('Invalid Input or Output Shape')
+                # kernel Size
+                kernel = math.ceil(feature_in.shape[3] / feature_out.shape[3])
+                # FLOPs
+                flops = kernel * kernel * feature_out.numel()
 
             # Upsample
             elif isinstance(module, nn.Upsample):
@@ -154,40 +124,38 @@ class Complexity():
                 # Nearest Interpolation
                 if module.mode == 'nearest':
                     # FLOPs
-                    flop = 0
+                    flops = 0
                 # Bilinear Interpolation
                 elif module.mode == 'bilinear':
-                    # Output Channels, Height and Width
-                    channel_out, height_out, width_out = feature_out.shape[1:]
                     # FLOPs
-                    flop = 4 * channel_out * height_out * width_out
+                    flops = 4 * feature_out.numel()
                 else:
                     raise TypeError('Invalid Mode for Upsample')
             
             # Nearest Interpolation
             elif isinstance(module, nn.UpsamplingNearest2d):
                 # FLOPs
-                flop = 0
+                flops = 0
 
             # Bilinear Interpolation
             elif isinstance(module, nn.UpsamplingBilinear2d):
-                # Output Channels, Height and Width
-                channel_out, height_out, width_out = feature_out.shape[1:]
                 # FLOPs
-                flop = 4 * channel_out * height_out * width_out
+                flops = 4 * feature_out.numel()
 
             # Flatten
             elif isinstance(module, nn.Flatten):
                 # FLOPs
-                flop = 0
+                flops = 0
 
             # Others
             else:
+                # Check Layer Name
+                print(module._get_name())
                 # FLOPs
-                flop = 0
+                flops = 0
 
             # Total FLOPs
-            self.flop += flop
+            self.flops += flops
 
             return
 
@@ -200,7 +168,6 @@ class Complexity():
             
             # Block is Indeed a Block
             if list(block.children()):
-
                 # Iterate Through Layer in Block
                 for child in block.children():
                     # Recursive Break Down
@@ -208,25 +175,19 @@ class Complexity():
 
             # Block is a Layer
             else:
-
                 # Register Hook
                 block.register_forward_hook(forward_hook)
 
                 return
-            
-        break_block(self.model)
                 
-        return
+        return break_block(self.model)
 
     """
     ====================================================================================================================
     Profile
     ====================================================================================================================
     """
-    def profile(self, order: str | Literal['G', 'M', 'k'] = 'M') -> None:
-
-        # Output Format
-        space = "{:<25}{:<20}"
+    def profile(self, order: str | Literal['G', 'M', 'k'] = 'M', num_input: int = 1) -> None:
 
         """
         ----------------------------------------------------------------------------------------------------------------
@@ -235,33 +196,34 @@ class Complexity():
         """
         # Dummy Input
         dummy = torch.rand((1, *self.dummy))
+        dummy = [dummy for _ in range(num_input)]
 
         # Start Time
         time_start = time.time()
 
         # Forward Pass
-        self.model(dummy)
+        for _ in range(10):
+            self.flops = 0
+            self.model(*dummy)
 
         # End Time
         time_end = time.time()
 
-        # FLOPs per Second
-        flops = self.flop / (time_end - time_start)
-        
-        # Output Log
-        print()
+        # FLOPs and FLOPs per Second
+        flops = self.flops
+        flops_per_sec = self.flops / ((time_end - time_start) / 10)
+
         if order == 'G':
-            print(space.format(round(self.flop * 1e-9, 3), 'GFLOPs'))
-            print(space.format(round(flops * 1e-9, 3), 'GFLOPS'))
+            flops = round(flops * 1e-9, 3)
+            flops_per_sec = round(flops_per_sec * 1e-9, 3)
         elif order == 'M':
-            print(space.format(round(self.flop * 1e-6, 3), 'MFLOPs'))
-            print(space.format(round(flops * 1e-6, 3), 'MFLOPS'))
+            flops = round(flops * 1e-6, 3)
+            flops_per_sec = round(flops_per_sec * 1e-6, 3)
         elif order == 'k':
-            print(space.format(round(self.flop * 1e-3, 3), 'kFLOPs'))
-            print(space.format(round(flops * 1e-3, 3), 'kFLOPS'))
+            flops = round(flops * 1e-3, 3)
+            flops_per_sec = round(flops_per_sec * 1e-3, 3)
         else:
             raise TypeError('Invalid Order')
-        print()
 
         """
         ----------------------------------------------------------------------------------------------------------------
@@ -272,10 +234,21 @@ class Complexity():
         num_params = sum(params.numel() for params in self.model.parameters())
         num_params_train = sum(params.numel() for params in self.model.parameters() if params.requires_grad)
 
+        """
+        ----------------------------------------------------------------------------------------------------------------
+        Output Log
+        ----------------------------------------------------------------------------------------------------------------
+        """
+        # Output Format
+        space = "{:^25}|{:^25}|{:^25}|{:^25}"
+
+        # Title
+        print('-------------------------------------------------------------------------------------------------------')
+        print(space.format(order + 'FLOPs', order + 'FLOPs', 'Total Parameters', 'Trainable Parameters'))
+        print('-------------------------------------------------------------------------------------------------------')
+
         # Output Log
-        print()
-        print(space.format(num_params, 'Total Parameters'))
-        print(space.format(num_params_train, 'Trainable Parameters'))
+        print(space.format(flops, flops_per_sec, num_params, num_params_train))
         print()
 
         return 
@@ -288,11 +261,28 @@ Main Function
 """
 if __name__ == '__main__':
 
-    # # Model
-    # model = Unet()
+    # Model
+    all_model = {
+                 'vgg11': models.vgg11(),
+                 'vgg13': models.vgg13(),
+                 'vgg16': models.vgg16(),
+                 'vgg19': models.vgg19(),
+                 'resnet18': models.resnet18(),
+                 'resnet34': models.resnet34(),
+                 'resnet50': models.resnet50(),
+                 'resnet101': models.resnet101(),
+                 'resnet152': models.resnet152(),
+                 'densenet121': models.densenet121(),
+                 'densenet169': models.densenet169(),
+                 'densenet201': models.densenet201(),
+                 'densenet161': models.densenet161(),
+                }
 
-    # # Compute Complexity
-    # calculator = Complexity(model, (7, 256, 256))
-    # calculator.profile()
+    for name, model in all_model.items():
 
-    pass
+        print()
+        print(name)
+
+        # Compute Complexity
+        calculator = Complexity(model, (3, 224, 224))
+        calculator.profile('G')
