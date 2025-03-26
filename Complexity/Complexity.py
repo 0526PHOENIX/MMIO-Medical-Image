8d9cd1f2079c1d8ff4c5a7168f27016c6287e4b9
Lab_Local <0526phoenix@gmail.com>
2025-03-26 18:23:58 +0800

    1823

--- Complexity/Complexity.py
+++ Complexity/Complexity.py
@@ -14,6 +14,8 @@ from torch import Tensor
 from torch.nn import Module
 
 from torchvision import models
+
+
 """
 ========================================================================================================================
 Complexity Calculator
@@ -26,11 +28,17 @@ class Complexity():
     Initialization
     ====================================================================================================================
     """
-    def __init__(self, model: Module, dummy: Tensor) -> None:
+    def __init__(self, model: Module, dummy: tuple[int, int, int], device: torch.device = None) -> None:
+
+        # Device: CPU or GPU
+        self.device = device or torch.device('cpu')
     
         # Model and Dummy Input
-        self.model = model
+        self.model = model.to(self.device)
         self.dummy = dummy
+
+        # Activations
+        self.actvs = 0
 
         # FLOPs
         self.flops = 0
@@ -57,14 +65,17 @@ class Complexity():
             # Conver Tuple to Tensor
             feature_in = feature_in[0]
 
+            # FLOPs
+            flops = None
+
             # Convolution 2D & Transpose Convolution 2D
             if isinstance(module, (nn.Conv2d, nn.ConvTranspose2d)):
                 # Input Channels
                 channel_in = feature_in.shape[1]
                 # Kernel Size
-                kernel = module.kernel_size[0]
+                kernel = module.kernel_size
                 # FLOPs
-                flops = 2 * kernel * kernel * channel_in * feature_out.numel()
+                flops = 2 * kernel[0] * kernel[1] * channel_in * feature_out.numel()
 
             # Fully Connected
             elif isinstance(module, nn.Linear):
@@ -85,11 +96,6 @@ class Complexity():
             elif isinstance(module, nn.GroupNorm):
                 # FLOPs
                 flops = 4 * feature_out.numel()
-
-            # # Layer Normalization (Self-Defined)
-            # elif isinstance(module, LayerNorm):
-            #     # FLOPs
-            #     flops = 7 * feature_out.numel()
 
             # Dropout
             elif isinstance(module, (nn.Dropout, nn.Dropout1d, nn.Dropout2d)):
@@ -112,7 +118,7 @@ class Complexity():
             elif isinstance(module, (nn.AdaptiveAvgPool2d, nn.AdaptiveMaxPool2d)):
                 # Check Shape
                 if (feature_in.shape[2] != feature_in.shape[3] or feature_out.shape[2] != feature_out.shape[3]):
-                    raise TypeError('Invalid Input or Output Shape')
+                    raise ValueError('Invalid Input or Output Shape')
                 # kernel Size
                 kernel = math.ceil(feature_in.shape[3] / feature_out.shape[3])
                 # FLOPs
@@ -120,7 +126,6 @@ class Complexity():
 
             # Upsample
             elif isinstance(module, nn.Upsample):
-
                 # Nearest Interpolation
                 if module.mode == 'nearest':
                     # FLOPs
@@ -130,7 +135,7 @@ class Complexity():
                     # FLOPs
                     flops = 4 * feature_out.numel()
                 else:
-                    raise TypeError('Invalid Mode for Upsample')
+                    raise ValueError('Invalid Mode for Upsample')
             
             # Nearest Interpolation
             elif isinstance(module, nn.UpsamplingNearest2d):
@@ -141,6 +146,14 @@ class Complexity():
             elif isinstance(module, nn.UpsamplingBilinear2d):
                 # FLOPs
                 flops = 4 * feature_out.numel()
+
+            # Pixel Shuffle
+            elif isinstance(module, nn.PixelShuffle):
+                # FLOPs
+                if module.upscale_factor == 2:
+                    flops = 0
+                elif module.upscale_factor == 3:
+                    flops = 9 * feature_in.shape[1] * feature_out.shape[1]
 
             # Flatten
             elif isinstance(module, nn.Flatten):
@@ -155,7 +168,8 @@ class Complexity():
                 flops = 0
 
             # Total FLOPs
-            self.flops += flops
+            self.actvs += feature_out.numel()
+            self.flops += flops or 0
 
             return
 
@@ -187,7 +201,7 @@ class Complexity():
     Profile
     ====================================================================================================================
     """
-    def profile(self, order: str | Literal['G', 'M', 'k'] = 'M', num_input: int = 1) -> None:
+    def profile(self, order: str | Literal['G', 'M', 'k'] = 'M', num_input: int = 1, batch_size: int = 16) -> None:
 
         """
         ----------------------------------------------------------------------------------------------------------------
@@ -195,7 +209,7 @@ class Complexity():
         ----------------------------------------------------------------------------------------------------------------
         """
         # Dummy Input
-        dummy = torch.rand((1, *self.dummy))
+        dummy = torch.rand((1, *self.dummy)).to(self.device)
         dummy = [dummy for _ in range(num_input)]
 
         # Start Time
@@ -203,16 +217,25 @@ class Complexity():
 
         # Forward Pass
         for _ in range(10):
+            self.actvs = 0
             self.flops = 0
             self.model(*dummy)
 
         # End Time
         time_end = time.time()
 
+        # Activations
+        actvs = self.actvs
+
         # FLOPs and FLOPs per Second
         flops = self.flops
         flops_per_sec = self.flops / ((time_end - time_start) / 10)
 
+        # Memory Cost
+        memory = int((actvs * 2 * 4 * batch_size) / (1024 ** 2))
+
+        # Magnitude
+        actvs = round(actvs * 1e-6, 3)
         if order == 'G':
             flops = round(flops * 1e-9, 3)
             flops_per_sec = round(flops_per_sec * 1e-9, 3)
@@ -223,7 +246,7 @@ class Complexity():
             flops = round(flops * 1e-3, 3)
             flops_per_sec = round(flops_per_sec * 1e-3, 3)
         else:
-            raise TypeError('Invalid Order')
+            raise ValueError('Invalid Order')
 
         """
         ----------------------------------------------------------------------------------------------------------------
@@ -231,8 +254,8 @@ class Complexity():
         ----------------------------------------------------------------------------------------------------------------
         """
         # Total Parameter & Trainable Parameter
-        num_params = sum(params.numel() for params in self.model.parameters())
-        num_params_train = sum(params.numel() for params in self.model.parameters() if params.requires_grad)
+        num_param = sum(param.numel() for param in self.model.parameters())
+        num_param_train = sum(param.numel() for param in self.model.parameters() if param.requires_grad)
 
         """
         ----------------------------------------------------------------------------------------------------------------
@@ -240,18 +263,19 @@ class Complexity():
         ----------------------------------------------------------------------------------------------------------------
         """
         # Output Format
-        space = "{:^25}|{:^25}|{:^25}|{:^25}"
+        title = "{:^21}|{:^21}|{:^21}|{:^21}|{:^21}|{:^21}"
+        space = "{:^21}|{:^21}|{:^21}|{:^21,}|{:^21,}|{:^21,}"
 
         # Title
-        print('-------------------------------------------------------------------------------------------------------')
-        print(space.format(order + 'FLOPs', order + 'FLOPs', 'Total Parameters', 'Trainable Parameters'))
-        print('-------------------------------------------------------------------------------------------------------')
+        print('-' * 130)
+        print(title.format(order + ' FLOPs', order + ' FLOPS', 'M Acts', 'Memory (MB)', 'Params', 'Trainable Params'))
+        print('-' * 130)
 
         # Output Log
-        print(space.format(flops, flops_per_sec, num_params, num_params_train))
+        print(space.format(flops, flops_per_sec, actvs, memory, num_param, num_param_train))
         print()
 
-        return 
+        return
 
 
 """
@@ -263,19 +287,19 @@ if __name__ == '__main__':
 
     # Model
     all_model = {
-                 'vgg11': models.vgg11(),
-                 'vgg13': models.vgg13(),
-                 'vgg16': models.vgg16(),
-                 'vgg19': models.vgg19(),
-                 'resnet18': models.resnet18(),
-                 'resnet34': models.resnet34(),
-                 'resnet50': models.resnet50(),
-                 'resnet101': models.resnet101(),
-                 'resnet152': models.resnet152(),
-                 'densenet121': models.densenet121(),
-                 'densenet169': models.densenet169(),
-                 'densenet201': models.densenet201(),
-                 'densenet161': models.densenet161(),
+                    'vgg11':       models.vgg11(),
+                    'vgg13':       models.vgg13(),
+                    'vgg16':       models.vgg16(),
+                    'vgg19':       models.vgg19(),
+                    'resnet18':    models.resnet18(),
+                    'resnet34':    models.resnet34(),
+                    'resnet50':    models.resnet50(),
+                    'resnet101':   models.resnet101(),
+                    'resnet152':   models.resnet152(),
+                    'densenet121': models.densenet121(),
+                    'densenet169': models.densenet169(),
+                    'densenet201': models.densenet201(),
+                    'densenet161': models.densenet161(),
                 }
 
     for name, model in all_model.items():
@@ -284,5 +308,5 @@ if __name__ == '__main__':
         print(name)
 
         # Compute Complexity
-        calculator = Complexity(model, (3, 224, 224))
-        calculator.profile('G')
+        calculator = Complexity(model, (3, 224, 224), torch.device('cuda'))
+        calculator.profile('G', num_input = 1, batch_size = 1)
